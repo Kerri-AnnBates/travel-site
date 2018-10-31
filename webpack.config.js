d3946c009f7efc9d83e965c51836103eb6c4cd95
Kerri-Ann <kaybee1jam@yahoo.com>
2018-10-30 22:16:25 -0400

    Add animation to the header for mobile via javascript complete

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    path: path.resolve(__dirname, "./app/temp/scripts"), //bundle our js
+    filename: "App.js"
+  },
+  module: { //convert our js files from es6 to regular js.
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        },
+        test: /\.js$/, //only want babel plugin to be applied to js files
+        exclude: /node-modules/
+      }
+    ]
+  }
+}
